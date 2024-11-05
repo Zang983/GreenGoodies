e64b2859a367aa501b22d8d0ffd682987dbc778c
Kerkar <kerkar.kevin@gmail.com>
2024-11-05 14:58:21 +0100

    Factories created

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Factory\OrderFactory;
+use App\Factory\OrderHasProductFactory;
+use App\Factory\ProductFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
+        $users = UserFactory::createMany(30, function () {
+            return [
+                "password" => $this->passwordHasher->hashPassword(new User(), "password")
+            ];});
+        $products = ProductFactory::createMany(30,function(){
+            return [
+              'picture' => random_int(1, 57) . '.jpg'
+            ];
+        });
+        $orders = OrderFactory::createMany(50);
+        $ordersHasProducts = OrderHasProductFactory::createMany(150, function () use ($products, $orders) {
+            return [
+                "product" => $products[array_rand($products)],
+                "order_reference" => $orders[array_rand($orders)]
+            ];
+        });
+
         // $manager->persist($product);
 
         $manager->flush();
---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Order;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Order>
+ */
+final class OrderFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Order::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'amount' => self::faker()->randomFloat(),
+            'createdAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
+            'shippingAddress' => self::faker()->text(255),
+            'status' => self::faker()->numberBetween(1, 4),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Order $order): void {})
+        ;
+    }
+}
